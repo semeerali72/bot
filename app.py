2a56d7db26002b6ac49ab6686dff997dfd7e1477
semeerali72 <semeerali72@gmail.com>
2017-03-02 21:08:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "modify":
+    if req.get("result").get("action") != "shipping.cost":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
