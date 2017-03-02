0ad21f8d75fa3eb1d428bf1c40e8333c42f4592c
semeerali72 <semeerali72@gmail.com>
2017-03-02 12:30:36 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "service funtion":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    services = parameters.get("services")
+
+    value = {'chaosmonkey':0, 'hystrix':1}
+
+    speech = "The value of output for  " + services + " is " + str(value[services])
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-resilient-bot"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=True, port=port, host='0.0.0.0')
